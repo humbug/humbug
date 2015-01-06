8c79552e058371aed28a5a3b596acbd9ffcb84a5
Padraic Brady <padraic.brady@gmail.com>
2015-01-06 23:19:38 +0000

    Add and enable arithmetic +=, -=, *=, etc mutators

---
+++ src/Humbug/Mutator/Arithmetic/DivEqual.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Humbug
+ *
+ * @category   Humbug
+ * @package    Humbug
+ * @copyright  Copyright (c) 2015 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    https://github.com/padraic/humbug/blob/master/LICENSE New BSD License
+ */
+
+namespace Humbug\Mutator\Arithmetic;
+
+use Humbug\Mutator\MutatorAbstract;
+
+class DivEqual extends MutatorAbstract
+{
+
+    /**
+     * Replace (/=) with (*=)
+     *
+     * @param array $tokens
+     * @param int $index
+     * @return array
+     */
+    public function getMutation(array $tokens, $index)
+    {
+        $tokens[$index] = [
+            T_MUL_EQUAL,
+            '*='
+        ];
+        return $tokens;
+    }
+
+    public static function mutates(array $tokens, $index)
+    {
+        $t = $tokens[$index];
+        if (is_array($t) && $t[0] == T_DIV_EQUAL) {
+            return true;
+        }
+        return false;
+    }
+
+}
