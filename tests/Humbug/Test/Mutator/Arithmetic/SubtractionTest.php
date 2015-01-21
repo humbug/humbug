88ddf9864e30375fc0cd1dea58b8dec2b7058bfa
Padraic Brady <padraic.brady@gmail.com>
2015-01-21 20:13:44 +0000

    Substraction to Subtraction renaming of test

---
+++ tests/Humbug/Test/Mutator/Arithmetic/SubtractionTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Humbug
+ *
+ * @category   Humbug
+ * @package    Humbug
+ * @subpackage UnitTests
+ * @copyright  Copyright (c) 2015 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    https://github.com/padraic/humbug/blob/master/LICENSE New BSD License
+ */
+
+namespace Humbug\Test\Mutator\Arithmetic;
+
+use Humbug\Mutator;
+
+class SubtractionTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testReturnsTokenEquivalentToAdditionOperator()
+    {
+        $mutation = new Mutator\Arithmetic\Subtraction;
+        $tokens = [];
+        $mutation->getMutation($tokens, 10);
+        $this->assertEquals([10 => '+'], $tokens);
+    }
+
+    public function testMutatesSubtractionOperatorToAdditionOperator()
+    {
+        $tokens = [10 => '-'];
+
+        $this->assertTrue(Mutator\Arithmetic\Subtraction::mutates($tokens, 10));
+
+        $tokens = [11 => '+'];
+
+        $this->assertFalse(Mutator\Arithmetic\Subtraction::mutates($tokens, 11));
+    }
+
+}
