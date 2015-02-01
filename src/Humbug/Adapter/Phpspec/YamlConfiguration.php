9c22c85dc6b7683dc996469ae6899011df017eec
Padraic Brady <padraic.brady@gmail.com>
2015-02-01 14:46:43 +0000

    Basic connections to phpspec adapter; to diverge code coverage vs phpspec mapping

--- src/Humbug/Adapter/Phpspec/YamlConfiguration.php
+++ src/Humbug/Adapter/Phpspec/YamlConfiguration.php
@@ -45,14 +45,95 @@ class YamlConfiguration extends ConfigurationAbstract
             self::$config['extensions'] = [];
         }
 
-        self::handleCodeCoverageLogging();
+        if (true === $firstRun) {
+            self::handleSpecMapLogging();
+            self::handleTimeCollectorLogging();
+        } else {
+            self::handleFilteredResourceLoader();
+            self::handleFastestSpecFirstFilter();
+            self::handleIncludeOnlySpecFilter($testSuites);
+        }
 
         $saveFile = self::$container->getCacheDirectory() . '/phpspec.humbug.yml';
+        $extensions = self::$config['extensions'];
+
+        /**
+         * Make extensions the final element to guarantee config is available
+         */
+        unset(self::$config['extensions']);
+        self::$config['extensions'] = $extensions;
         $yaml = Yaml::dump(self::$config, 5);
         file_put_contents($saveFile, $yaml);
         return $saveFile;
     }
 
+    protected static function handleFilteredResourceLoader()
+    {
+        self::$config['extensions'][] = 'Humbug\PhpSpec\FilteredResourceLoaderExtension';
+        if (!isset(self::$config['humbug.filtered_resource_loader.filters'])
+        || !is_array(self::$config['humbug.filtered_resource_loader.filters'])) {
+            self::$config['humbug.filtered_resource_loader.filters'] = [];
+        }
+    }
+
+    protected static function handleFastestSpecFirstFilter()
+    {
+        self::$config['humbug.filtered_resource_loader.filters'][] =
+            'Humbug\PhpSpec\Loader\Filter\Specification\FastestFirstFilter';
+        self::$config['humbug.time_collector.target'] =
+            self::$container->getCacheDirectory() . '/phpspec.times.humbug.json';
+    }
+
+    protected static function handleIncludeOnlySpecFilter(array $specs)
+    {
+
+    }
+
+    protected static function handleSpecMapLogging()
+    {
+        self::$config['extensions'][] = 'Humbug\PhpSpec\SpecMapperExtension';
+        self::$config['humbug.spec_mapper.target'] =
+            self::$container->getCacheDirectory() . '/phpspec.specmap.humbug.json';
+    }
+
+    protected static function handleTimeCollectorLogging()
+    {
+        self::$config['extensions'][] = 'Humbug\PhpSpec\TimeCollectorExtension';
+        self::$config['humbug.time_collector.target'] =
+            self::$container->getCacheDirectory() . '/phpspec.times.humbug.json';
+    }
+
+    protected static function parseConfigurationFile()
+    {
+        $paths = array('phpspec.yml','phpspec.yml.dist');
+
+        $config = array();
+        foreach ($paths as $path) {
+            if ($path && file_exists($path) && $parsedConfig = Yaml::parse(file_get_contents($path))) {
+                $config = $parsedConfig;
+                break;
+            }
+        }
+
+        if ($homeFolder = getenv('HOME')) {
+            $localPath = $homeFolder.'/.phpspec.yml';
+            if (file_exists($localPath) && $parsedConfig = Yaml::parse(file_get_contents($localPath))) {
+                $config = array_replace_recursive($parsedConfig, $config);
+            }
+        }
+
+        return $config;
+    }
+
+    /**
+     * This can set up code coverage, but its usefulness is only as good as the
+     * Code Coverage extension and specs, and results have been less than desireable.
+     *
+     * It has been replaced by handleSpecMapLogging() which uses a simple class->spec
+     * mapping.
+     *
+     * @deprecated
+     */
     protected static function handleCodeCoverageLogging()
     {
         if (!isset(self::$config['code_coverage'])
@@ -88,27 +169,7 @@ class YamlConfiguration extends ConfigurationAbstract
                 self::$config['code_coverage']['blacklist'][] = realpath($d);
             }
         }
-    }
 
-    protected static function parseConfigurationFile()
-    {
-        $paths = array('phpspec.yml','phpspec.yml.dist');
-
-        $config = array();
-        foreach ($paths as $path) {
-            if ($path && file_exists($path) && $parsedConfig = Yaml::parse(file_get_contents($path))) {
-                $config = $parsedConfig;
-                break;
-            }
-        }
-
-        if ($homeFolder = getenv('HOME')) {
-            $localPath = $homeFolder.'/.phpspec.yml';
-            if (file_exists($localPath) && $parsedConfig = Yaml::parse(file_get_contents($localPath))) {
-                $config = array_replace_recursive($parsedConfig, $config);
-            }
-        }
-
-        return $config;
+        self::$config['code_coverage']['show_uncovered_files'] = false;
     }
 }
