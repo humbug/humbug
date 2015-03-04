7951dba7f17c1d01ad928069f95f5b9a34a74f97
Thibaud Fabre <aztech-dev@users.noreply.github.com>
2015-03-04 23:16:57 +0100

    CS fix on docblock
    [skip ci]

--- src/Collector.php
+++ src/Collector.php
@@ -9,7 +9,7 @@ namespace Humbug;
  * @package    Humbug
  * @copyright  Copyright (c) 2015 Pádraic Brady (http://blog.astrumfutura.com)
  * @license    https://github.com/padraic/humbug/blob/master/LICENSE New BSD License
- * @author Thibaud Fabre
+ * @author     Thibaud Fabre
  */
 class Collector
 {
