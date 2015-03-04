a1e72ad23e6aee3187e4729dc15d94578db7a80b
Thibaud Fabre <aztech-dev@users.noreply.github.com>
2015-03-04 23:16:23 +0100

    Add license
    [skip ci]

--- src/Collector.php
+++ src/Collector.php
@@ -5,7 +5,10 @@ namespace Humbug;
 /**
  * Class collecting all mutants and their results.
  *
- * @package Humbug
+ * @category   Humbug
+ * @package    Humbug
+ * @copyright  Copyright (c) 2015 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    https://github.com/padraic/humbug/blob/master/LICENSE New BSD License
  * @author Thibaud Fabre
  */
 class Collector
