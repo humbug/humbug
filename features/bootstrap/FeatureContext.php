4b7158b549820b67b85788e50615f8f288f576aa
Padraic Brady <padraic.brady@gmail.com>
2015-03-08 02:58:25 +0000

    Add --version behat feature to tee off

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,12 +4,25 @@ use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
+
+use Symfony\Component\Console\Tester\ApplicationTester;
+use Humbug\Console\Application;
+use Humbug\Command\Humbug as HumbugCommand;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context, SnippetAcceptingContext
 {
+
+
+    private $application;
+
+    private $appTester;
+
+    private $startingDirectory;
+
     /**
      * Initializes context.
      *
@@ -19,5 +32,57 @@ class FeatureContext implements Context, SnippetAcceptingContext
      */
     public function __construct()
     {
+        $this->application = new Application;
+        $this->application->setAutoExit(false);
+        $this->application->add(new HumbugCommand);
+        $this->appTester = new ApplicationTester($this->application);
+    }
+
+    /**
+     * @beforeScenario
+     */
+    public function retainStartingDirectory()
+    {
+        $this->startingDirectory = getcwd();
+    }
+
+    /**
+     * @afterScenario
+     */
+    public function restoreWorkingDirectory()
+    {
+        if ($this->startingDirectory !== getcwd()) {
+            chdir($this->startingDirectory);
+        }
+    }
+
+    /**
+     * @Given I am in any directory
+     */
+    public function iAmInAnyDirectory()
+    {
+        chdir(sys_get_temp_dir());
+    }
+
+    /**
+     * @When I run humbug with :arg1
+     */
+    public function iRunHumbugWith($arg1)
+    {
+        $arguments = explode(' ', $arg1);
+        $this->appTester->run($arguments);
+    }
+
+    /**
+     * @Then I should see
+     */
+    public function iShouldSee(PyStringNode $string)
+    {
+        $string = (string) $string;
+        if (trim($this->appTester->getDisplay()) !== trim($string)) {
+            throw new \Exception(sprintf(
+                'Actual output was:%s%s', PHP_EOL, $string
+            ));
+        }
     }
 }
---
+++ features/version.feature
@@ -0,0 +1,17 @@
+Feature: Developer runs Humbug with --version option
+    In order to know the version
+    As a Humbug user
+    I need to be able to read the version
+
+    Scenario: View Humbug version
+        Given I am in any directory
+        When I run humbug with "--version"
+        Then I should see
+            """
+             _  _            _
+            | || |_  _ _ __ | |__ _  _ __ _
+            | __ | || | '  \| '_ \ || / _` |
+            |_||_|\_,_|_|_|_|_.__/\_,_\__, |
+                                      |___/ 
+            Humbug version 1.0-dev
+            """
