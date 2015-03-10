c058280c3189edca4626410cd2ff158a5607bd76
rollen <rafal.wartalski@gmail.com>
2015-03-11 00:03:14 +0100

    Missing licence information

--- src/Command/Configure.php
+++ src/Command/Configure.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Humbug
+ *
+ * @category   Humbug
+ * @package    Humbug
+ * @copyright  Copyright (c) 2015 Pádraic Brady (http://blog.astrumfutura.com)
+ * @license    https://github.com/padraic/humbug/blob/master/LICENSE New BSD License
+ *
+ * @author     rafal.wartalski@gmail.com
+ */
 
 namespace Humbug\Command;
 
