6926bedbe2b8b7bfc34f15aa3529795149ce4c6a
Padraic Brady <padraic.brady@gmail.com>
2015-04-03 04:02:20 +0100

    Add feedback to diagnose Travis failure (passing locally...)

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -93,7 +93,11 @@ class FeatureContext implements Context, SnippetAcceptingContext
         $output = $this->appTester->getDisplay();
         if (trim($output) !== trim($string)) {
             throw new \RuntimeException(sprintf(
-                'Output difference:%s%s', PHP_EOL, $this->differ->diff(trim($string), trim($output))
+                'Output difference:%s%s%sHumbug Log:%s',
+                PHP_EOL,
+                $this->differ->diff(trim($string), trim($output)),
+                PHP_EOL,
+                file_get_contents(getcwd() . '/humbuglog.txt')
             ));
         }
     }
