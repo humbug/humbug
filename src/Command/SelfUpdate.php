65e9c6201eb97ad0482ef9f1d30824b544c5908c
Padraic Brady <padraic.brady@gmail.com>
2015-05-20 20:29:45 +0100

    Implement new updater logic to migrate automatically to any alpha/beta/rc versions on Github

--- src/Command/SelfUpdate.php
+++ src/Command/SelfUpdate.php
@@ -10,21 +10,30 @@
 
 namespace Humbug\Command;
 
+use Humbug\Exception\FilesystemException;
+use Humbug\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Helper\ProgressBar;
-use Symfony\Component\Finder\Finder;
-use Humbug\Exception\FilesystemException;
 use Humbug\SelfUpdate\Updater;
+use Humbug\SelfUpdate\VersionParser;
 
 class SelfUpdate extends Command
 {
 
-    const VERSION = 'https://padraic.github.io/humbug/downloads/humbug.version';
+    const VERSION_URL = 'https://padraic.github.io/humbug/downloads/humbug.version';
 
-    const PHAR = 'https://padraic.github.io/humbug/downloads/humbug.phar';
+    const PHAR_URL = 'https://padraic.github.io/humbug/downloads/humbug.phar';
+
+    const PACKAGE_NAME = 'humbug/humbug';
+
+    const FILE_NAME = 'humbug.phar';
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
 
     /**
      * Execute the command.
@@ -34,38 +43,116 @@ class SelfUpdate extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->output = $output;
+        /**
+         * This specific code assumes migration from manual SHA-1 tracked
+         * development versions to either a pre-release (default) or solely
+         * a stable version. An option is allowed for going back to manual
+         * phar builds at the development level.
+         *
+         * Will be updated again once the stable track is established.
+         */
+        if ($input->getOption('dev')) {
+            $this->updateToDevelopmentBuild();
+        }
+        /**
+         * This is the final development build being installed automatically
+         * Any future dev updates need to carry the --dev flag in command.
+         */
+        $this->updateToPreReleaseBuild(); // alpha/beta/rc?
+
+        // Can likely add update config at some point...
+    }
+
+    protected function updateToStableBuild()
+    {
         $updater = new Updater;
-        $updater->setPharUrl(self::PHAR);
-        $updater->setVersionUrl(self::VERSION);
+        $updater->setStrategy(Updater::STRATEGY_GITHUB);
+        $this->updateUsingGithubReleases($updater);
+    }
+
+    protected function updateToPreReleaseBuild()
+    {
+        $updater = new Updater;
+        $updater->setStrategy(Updater::STRATEGY_GITHUB);
+        $updater->getStrategy()->setStability('unstable');
+        $this->updateUsingGithubReleases($updater);
+    }
+
+    protected function updateToDevelopmentBuild()
+    {
+        $updater = new Updater;
+        $updater->getStrategy()->setPharUrl(self::PHAR_URL);
+        $updater->getStrategy()->setVersionUrl(self::VERSION_URL);
+        $this->update($updater);
+    }
+
+    protected function updateUsingGithubReleases(Updater $updater)
+    {
+        $updater->getStrategy()->setPackageName(self::PACKAGE_NAME);
+        $updater->getStrategy()->setPharName(self::FILE_NAME);
+        $updater->getStrategy()->setCurrentLocalVersion(Application::VERSION);
+        $this->update($updater);
+    }
+
+    protected function update(Updater $updater)
+    {
+        $this->output->writeln('Updating...'.PHP_EOL);
         try {
             $result = $updater->update();
             if ($result) {
-                $output->writeln('<fg=green>Humbug has been updated.</fg=green>');
-                $output->writeln(sprintf(
-                    '<fg=green>Current SHA-1 hash is:</fg=green> <options=bold>%s</options=bold>.',
+                $this->output->writeln('<fg=green>Humbug has been updated.</fg=green>');
+                $this->output->writeln(sprintf(
+                    '<fg=green>Current version is:</fg=green> <options=bold>%s</options=bold>.',
                     $updater->getNewVersion()
                 ));
-                $output->writeln(sprintf(
-                    '<fg=green>Previous SHA-1 hash was:</fg=green> <options=bold>%s</options=bold>.',
+                $this->output->writeln(sprintf(
+                    '<fg=green>Previous version was:</fg=green> <options=bold>%s</options=bold>.',
                     $updater->getOldVersion()
                 ));
             } else {
-                $output->writeln('<fg=green>Humbug is currently up to date.</fg=green>');
-                $output->writeln(sprintf(
-                    '<fg=green>Current SHA-1 hash is:</fg=green> <options=bold>%s</options=bold>.',
+                $this->output->writeln('<fg=green>Humbug is currently up to date.</fg=green>');
+                $this->output->writeln(sprintf(
+                    '<fg=green>Current version is:</fg=green> <options=bold>%s</options=bold>.',
                     $updater->getOldVersion()
                 ));
             }
         } catch (\Exception $e) {
-            $output->writeln(sprintf('Error: <fg=yellow>%s</fg=yellow>', $e->getMessage()));
+            $this->output->writeln(sprintf('Error: <fg=yellow>%s</fg=yellow>', $e->getMessage()));
         }
+        $this->output->write(PHP_EOL);
+        $this->output->writeln('You can also select update stability using --dev, --pre (alpha/beta/rc) or --stable.');
     }
 
     protected function configure()
     {
         $this
             ->setName('self-update')
-            ->setDescription('Update humbug.phar to current version')
+            ->setDescription('Update humbug.phar to most recent stable, pre-release or development build.')
+            ->addOption(
+               'dev',
+               'd',
+               InputOption::VALUE_NONE,
+               'Update to most recent development build of Humbug.'
+            )
+            ->addOption(
+               'pre',
+               'p',
+               InputOption::VALUE_NONE,
+               'Update to most recent pre-release version of Humbug (alpha/beta/rc), if higher than stable versions, tagged on Github.'
+            )
+            ->addOption(
+               'stable',
+               's',
+               InputOption::VALUE_NONE,
+               'Update to most recent stable version.'
+            )
+            ->addOption(
+               'check',
+               'c',
+               InputOption::VALUE_NONE,
+               'Checks what updates are available across all possible stability tracks.'
+            )
         ;
     }
 }
